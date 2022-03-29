6531eed827e3d76311731f905b42b46d559683a6
consuelita <adelita@operatorfoundation.org>
2022-03-29 13:19:19 -0500

    Update ShadowController.swift

--- Sources/ShapeshifterDispatcherSwift/ShadowController.swift
+++ Sources/ShapeshifterDispatcherSwift/ShadowController.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Logging
+import Crypto
 
 import ShadowSwift
 
@@ -30,13 +31,26 @@ struct ShadowController
         
         print("Created a config from JSON:")
         print(shadowConfig.mode)
-        print(shadowConfig.password)
         print(shadowConfig.serverIP)
         print(shadowConfig.port)
         
+        guard let serverPersistentPrivateKeyData = Data(hex: shadowConfig.password) else
+        {
+            //.error("ShShD Failed to parse password from config.")
+            return
+        }
+        print("ShShD Parsed a config file and got some key data.")
+        
+        guard let serverPersistentPrivateKey = try? P256.KeyAgreement.PrivateKey(rawRepresentation: serverPersistentPrivateKeyData) else
+        {
+            //logger.error("ShShD Failed to generate key from data.")
+            return
+        }
+        print("ShShD Generated a public key from some data \(serverPersistentPrivateKey.publicKey.compactRepresentation!.hex)")
+        
         guard let shadowListener = ShadowServer(host: bindHost, port: bindPort, config: shadowConfig, logger: appLog) else
         {
-            appLog.error("Failed to initialize a ShadowServer with the config provided.")
+            appLog.error("ShShD Failed to initialize a ShadowServer with the config provided.")
             return
         }
         
