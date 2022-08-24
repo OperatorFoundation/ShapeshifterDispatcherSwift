4c16143e7f22a862931ff61d46bbd42e85553dab
consuelita <adelita@operatorfoundation.org>
2022-08-23 20:34:55 -0500

    Better Routing

--- Sources/ShapeshifterDispatcherSwift/RoutingController.swift
+++ Sources/ShapeshifterDispatcherSwift/RoutingController.swift
@@ -6,13 +6,11 @@
 //
 
 import Foundation
-
 import Transmission
 
 class RoutingController
 {
-    // Could be tuned through testing in the future
-    let maxReadSize = 2048
+    var routes = [Router]()
     
     func handleListener(listener: Transmission.Listener, targetHost: String, targetPort: Int)
     {
@@ -30,17 +28,8 @@ class RoutingController
                     continue
                 }
                 
-                print("Received a new connection 🌷")
-                let targetToTransportQueue = DispatchQueue(label: "targetToTransportQueue")
-                let transportToTargetQueue = DispatchQueue(label: "transportToTargetQueue")
-                
-                targetToTransportQueue.async {
-                    self.transferTargetToTransport(transportConnection: transportConnection, targetConnection: targetConnection)
-                }
-                
-                transportToTargetQueue.async {
-                    self.transferTransportToTarget(transportConnection: transportConnection, targetConnection: targetConnection)
-                }
+                let route = Router(controller: self, transportConnection: transportConnection, targetConnection: targetConnection)
+                routes.append(route)
             }
             catch
             {
@@ -51,53 +40,127 @@ class RoutingController
         }
     }
     
+    func remove(route: Router)
+    {
+        self.routes = self.routes.filter
+        {
+            otherRoute in
+            
+            otherRoute != route
+        }
+    }
+}
+
+class Router
+{
+    let maxReadSize = 2048 // Could be tuned through testing in the future
+    let transportConnection: Transmission.Connection
+    let targetConnection: Transmission.Connection
+    let targetToTransportQueue = DispatchQueue(label: "targetToTransportQueue")
+    let transportToTargetQueue = DispatchQueue(label: "transportToTargetQueue")
+    let cleanupQueue = DispatchQueue(label: "cleanupQueue")
+    let lock = DispatchSemaphore(value: 2)
+    let controller: RoutingController
+    let uuid = UUID()
+    
+    var keepGoing = true
+    
+    init(controller: RoutingController, transportConnection: Transmission.Connection, targetConnection: Transmission.Connection)
+    {
+        self.controller = controller
+        self.transportConnection = transportConnection
+        self.targetConnection = targetConnection
+        
+        print("Dispatcher Router Received a new connection")
+        
+        targetToTransportQueue.async {
+            self.transferTargetToTransport(transportConnection: transportConnection, targetConnection: targetConnection)
+        }
+        
+        transportToTargetQueue.async {
+            self.transferTransportToTarget(transportConnection: transportConnection, targetConnection: targetConnection)
+        }
+        
+        cleanupQueue.async {
+            self.cleanup()
+        }
+    }
+    
     func transferTargetToTransport(transportConnection: Transmission.Connection, targetConnection: Transmission.Connection)
     {
-        while true
+        while keepGoing
         {
             guard let dataFromTarget = targetConnection.read(maxSize: maxReadSize) else
             {
                 appLog.debug("transferTargetToTransport: Received no data from the target on read.")
-                return
+                keepGoing = false
+                break
             }
 
             guard dataFromTarget.count > 0 else
             {
                 appLog.error("transferTargetToTransport: 0 length data was read - this should not happen")
-                return
+                keepGoing = false
+                break
             }
                         
             guard transportConnection.write(data: dataFromTarget) else
             {
                 appLog.debug("transferTargetToTransport: Unable to send target data to the transport connection. The connection was likely closed.")
-                return
+                keepGoing = false
+                break
             }
         }
+        
+        lock.signal()
     }
     
     func transferTransportToTarget(transportConnection: Transmission.Connection, targetConnection: Transmission.Connection)
     {
-        while true
+        while keepGoing
         {
             print("transferTransportToTarget: Attempting to read...")
             guard let dataFromTransport = transportConnection.read(maxSize: maxReadSize) else
             {
                 appLog.debug("transferTransportToTarget: Received no data from the target on read.")
-                return
+                keepGoing = false
+                break
             }
             print("transferTransportToTarget: Finished reading.")
             
             guard dataFromTransport.count > 0 else
             {
                 appLog.error("transferTransportToTarget: 0 length data was read - this should not happen")
-                return
+                keepGoing = false
+                break
             }
             
             guard targetConnection.write(data: dataFromTransport) else
             {
                 appLog.debug("transferTransportToTarget: Unable to send target data to the target connection. The connection was likely closed.")
-                return
+                keepGoing = false
+                break
             }
         }
+        
+        lock.signal()
+    }
+    
+    func cleanup()
+    {
+        lock.wait()
+        
+        print("Dispatcher Route cleaning up...")
+        controller.remove(route: self)
+        targetConnection.close()
+        transportConnection.close()
+    }
+}
+
+extension Router: Equatable
+{
+    static func == (lhs: Router, rhs: Router) -> Bool
+    {
+        return lhs.uuid == rhs.uuid
     }
 }
