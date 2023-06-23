ff1aba862cf28272f354f7bd6971f2f95e53b160
Jessica9-star <68959096+Jessica9-star@users.noreply.github.com>
2023-06-22 22:55:13 -0500

    Updated Package version to .macOS(.v13)

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "ShapeshifterDispatcherSwift",
     platforms: [
-        .macOS(.v12)
+        .macOS(.v13)
     ],
     dependencies: [
         .package(url:"https://github.com/OperatorFoundation/ReplicantSwift.git", branch: "main"),
